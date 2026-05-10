396c39640d755fe5c597f8c42cf1494b4bfc5e15
pointp <65812086+MovingPointP@users.noreply.github.com>
2026-05-11 01:15:57 +0900

    add: デフォルトテンプレートを定義

---
+++ src/constants/defaultTemplate.ts
@@ -0,0 +1,16 @@
+// テンプレートファイル（rejour/templates/default.md）が存在しない場合のフォールバック
+export const DEFAULT_TEMPLATE = `---
+tags: []
+---
+
+# {{date}}（{{day}}）
+
+## 気分
+⭐⭐⭐⭐⭐
+
+## 今日の出来事
+
+
+## 感じたこと
+
+`;
